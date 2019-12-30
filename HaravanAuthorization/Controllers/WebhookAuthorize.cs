7f756bd2aee566fdfb60fb48ddcec96ada78785f
Cường Lưu Mạnh <cuong.luumanh@haravan.com>
2019-12-30 17:23:00 +0700

    Add webhook

---
+++ HaravanAuthorization/Controllers/WebhookAuthorize.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaravanAuthorization.Controllers
+{
+    public class WebhookAuthorize : Attribute, IAsyncActionFilter
+    {
+        private readonly string clientSecret;
+
+        public WebhookAuthorize()
+        {
+            clientSecret = "cac97ec3b4414b066acd665d3cc3571a4cd216925d3bbafdf642d770126e19b5";
+        }
+
+        private string HashHmacSHA1(string originalData, string secretKey)
+        {
+            var sha1 = null as byte[];
+
+            using (var crypto = new HMACSHA1(Encoding.UTF8.GetBytes(secretKey)))
+                sha1 = crypto.ComputeHash(Encoding.UTF8.GetBytes(originalData));
+
+            var hashString = new StringBuilder();
+
+            foreach (var b in sha1)
+                hashString.Append(b.ToString("x2"));
+
+            var result = hashString.ToString();
+
+            return result;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var request = context.HttpContext.Request;
+
+            var hmac = (string)request.Headers["X-Haravan-Hmac-Sha256"];
+
+            if (string.IsNullOrWhiteSpace(hmac))
+            {
+                context.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            using (var reader = new StreamReader(request.Body))
+            {
+                var body_string = await reader.ReadToEndAsync();
+
+                var signature = HashHmacSHA1(body_string, clientSecret);
+
+                if (signature != hmac)
+                {
+                    context.Result = new StatusCodeResult(401);
+                    return;
+                }
+
+                context.ActionArguments.Add("topic", (string)request.Headers["X-Haravan-Topic"]);
+                context.ActionArguments.Add("orgId", (string)request.Headers["X-Haravan-Org-Id"]);
+                context.ActionArguments.Add("body", body_string);
+                context.ActionArguments.Add("test", request.Headers.ContainsKey("X-Haravan-Test"));
+            }
+
+            await next();
+        }
+    }
+}
---
+++ HaravanAuthorization/Controllers/WebhookController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaravanAuthorization.Controllers
+{
+    [Route("api/[controller]")]
+    public partial class WebhookController : Controller
+    {
+        // POST api/values
+        [ServiceFilter(typeof(WebhookAuthorize))]
+        [HttpPost]
+        public async Task Post(string orgId, string topic, string body, bool test)
+        {
+            switch (topic)
+            {
+                case "products/update":
+                    break;
+                case "products/deleted":
+                    break;
+                case "app/uninstalled":
+                    break;
+            }
+        }
+
+        [HttpGet]
+        public string VerifyWebhook()
+        {
+            string verify_token = HttpContext.Request.Query["hub.verify_token"].ToString();
+
+            string challenge = HttpContext.Request.Query["hub.challenge"].ToString();
+
+            if (verify_token == "cac97ec3b4414b066acd665d3cc3571a4cd216925d3bbafdf642d770126e19b5")
+                return challenge;
+
+            return null;
+        }
+    }
+}
